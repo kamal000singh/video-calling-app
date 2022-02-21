174fe66dd19f858598d60f38806364ebc5226cd1
kamal-singh <kamal000rawat@gmail.com>
2022-02-21 17:27:25 +0530

    21-02-22 add audio features

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -2,18 +2,33 @@ import { Button } from '@material-ui/core';
 import { AddIcCall, Close } from '@material-ui/icons';
 import React, { useContext } from 'react'
 import { SocketContext } from '../contextAPI/SocketContext';
+import ring from '../audio/ring.mp3';
 
 const Notifications = () => {
   const { answerCall, call, callAccepted, rejectCall } = useContext(SocketContext);
-
+  let ringtone = new Audio(ring);
+  let setRing;
+  if (call.isReceivedCall && !callAccepted) {
+    setRing = setInterval(() => {
+      ringtone.play();
+    }, 1500);
+  }
+  const handleCall = () => {
+    answerCall();
+    clearInterval(setRing);
+  }
   return (
     <>
-      {call.isReceivedCall && !callAccepted && (<div className="d-flex justify-content-evenly p-2">
-        <h3>{call.name} is calling...</h3>
-        <Button variant="contained" color="primary" onClick={answerCall} startIcon={<AddIcCall />} >Accept</Button><Button variant='contained' color='secondary' startIcon={<Close />} onClick={rejectCall} >Reject</Button>
-      </div>)}
+      {call.isReceivedCall && !callAccepted &&
+        (<div className="callReceive text-light border rounded-3">
+          <h3>{call.name} is calling...</h3>
+          <div className="">
+            <Button variant="contained" className="m-2" color="primary" onClick={handleCall} startIcon={<AddIcCall />} >Accept</Button><Button variant='contained' color='secondary' className="m-2" startIcon={<Close />} onClick={rejectCall} >Reject</Button>
+          </div>
+
+        </div>)
+      }
     </>
   )
 }
-
-export default Notifications
+export default Notifications;
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -7,18 +7,18 @@ const VideoPlayer = () => {
     const { name, callAccepted, callEnded, stream, call, myVideo, userVideo } = useContext(SocketContext);
 
     return (
-        <div className="mt-4">
-            <div className={`row justify-content-center`}>
-                {stream && <div className={`col col-md-5 m-1 bg-light border rounded-3`}>
-                    <Grid item xs={12} md={12}>
-                        <Typography variant="h5" className="text-start px-1" gutterBottom >{name || 'Name'}</Typography>
-                        <video style={{ width: '100%', height: '100%' }} playsInline muted ref={myVideo} autoPlay />
+        <div className="container mt-4">
+            <div className={`row justify-content-center `}>
+                {stream && <div className={`col-12 col-md-6 p-2`}>
+                    <Grid className='border rounded-3 bg-light' item xs={12} md={12}>
+                        <Typography variant="h5" className="text-start px-2" gutterBottom >{name || 'Name'}</Typography>
+                        <video style={{ width: '100%', height: '100%', padding: '5px' }} playsInline muted ref={myVideo} autoPlay />
                     </Grid>
                 </div>}
-                {callAccepted && !callEnded && <div className={`col col-md-5  m-1 bg-light border rounded-3`}>
-                    <Grid item xs={12} md={12}>
-                        <Typography variant="h5" className="text-start px-1" gutterBottom >{call.name || 'Name'}</Typography>
-                        <video style={{ width: '100%', height: '100%' }} playsInline muted ref={userVideo} autoPlay />
+                {callAccepted && !callEnded && <div className={`col-12 col-md-6 p-2`}>
+                    <Grid className='border rounded-3 bg-light' item xs={12} md={12}>
+                        <Typography variant="h5" className="text-start px-2" gutterBottom >{call.name || 'Name'}</Typography>
+                        <video style={{ width: '100%', height: '100%', padding: '5px' }} playsInline muted ref={userVideo} autoPlay />
                     </Grid>
                 </div>}
 
