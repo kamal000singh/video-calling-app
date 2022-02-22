781f33a3dabf55d19b4e754f4afd168a3422b078
kamal-singh <kamal000rawat@gmail.com>
2022-02-22 14:47:07 +0530

    22-02-22 enable audio edit

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -5,7 +5,7 @@ import { SocketContext } from '../contextAPI/SocketContext';
 import ring from '../audio/ring.mp3';
 
 const Notifications = () => {
-  const { answerCall, call, callAccepted, rejectCall } = useContext(SocketContext);
+  const { answerCall, call, name, callAccepted, rejectCall } = useContext(SocketContext);
   let ringtone = new Audio(ring);
   let setRing;
   if (call.isReceivedCall && !callAccepted) {
@@ -13,15 +13,15 @@ const Notifications = () => {
       ringtone.play();
     }, 1500);
   }
-  const handleCall = () => {
-    answerCall();
-    clearInterval(setRing);
+  const handleCall = async () => {
+    await clearInterval(setRing);
+    await answerCall();
   }
   return (
     <>
       {call.isReceivedCall && !callAccepted &&
         (<div className="callReceive text-light border rounded-3">
-          <h3>{call.name} is calling...</h3>
+          <h3>{name} is calling...</h3>
           <div className="">
             <Button variant="contained" className="m-2" color="primary" onClick={handleCall} startIcon={<AddIcCall />} >Accept</Button><Button variant='contained' color='secondary' className="m-2" startIcon={<Close />} onClick={rejectCall} >Reject</Button>
           </div>
