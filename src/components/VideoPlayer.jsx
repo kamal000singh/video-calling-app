75aa3b18c8ced5eb84070a1cc3c9a2265cc857ae
kamal-singh <kamal000rawat@gmail.com>
2022-02-22 18:57:05 +0530

    22-02-22 4rd commit

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -12,13 +12,13 @@ const VideoPlayer = () => {
                 {stream && <div className={`col-12 col-md-6 p-2`}>
                     <Grid className='border rounded-3 bg-light' item xs={12} md={12}>
                         <Typography variant="h5" className="text-start px-2" gutterBottom >{name || 'Name'}</Typography>
-                        <video style={{ width: '100%', height: '100%', padding: '5px' }} playsInline muted={myVideo ? true : false} ref={myVideo} autoPlay />
+                        <video style={{ width: '100%', height: '100%', padding: '5px' }} playsInline muted={true} ref={myVideo} autoPlay />
                     </Grid>
                 </div>}
                 {callAccepted && !callEnded && <div className={`col-12 col-md-6 p-2`}>
                     <Grid className='border rounded-3 bg-light' item xs={12} md={12}>
                         <Typography variant="h5" className="text-start px-2" gutterBottom >{call.name || 'Name'}</Typography>
-                        <video style={{ width: '100%', height: '100%', padding: '5px' }} playsInline muted={userVideo ? false : true} ref={userVideo} autoPlay />
+                        <video style={{ width: '100%', height: '100%', padding: '5px' }} playsInline ref={userVideo} autoPlay />
                     </Grid>
                 </div>}
 
